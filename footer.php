5d4b467508ac049cbd5b08b61da071788d2dd7fa
korg <mr.korg@ya.ru>
2017-02-10 10:27:19 +0200

    add footer

--- footer.php
+++ footer.php
@@ -1,6 +1,23 @@
 
     </div>
 
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6">
+                    <p class="copyright">&#169; 2017 всі права захищені</p>
+                </div>
+                <div class="col-xs-12 col-sm-6">
+                    <p class="studio">
+                        <a href="http://astudio.ck.ua/" target="_blank">
+                            Розробка сайту Веб-студія "Astudio"
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
     <?php wp_footer(); ?>
     
 </body>
